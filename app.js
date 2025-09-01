cf503263c0aef009b468b1a0ef952557ce177747
agrajjain4786 <agrajjain4786@gmail.com>
2025-09-01 14:40:32 +0530

    done basic crud operations write apis and connected with database

--- app.js
+++ app.js
@@ -1,10 +1,76 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const method = require("method-override");
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(method("_method"));
 app.get("/", (req, res) => {
   res.send("root");
 });
+// index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
+
+// new Listing Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+// create route
+app.post("/listings", async (req, res) => {
+  const newlisting = new Listing(req.body.listing);
+  await newlisting.save();
+  res.redirect("/listings");
+});
+
+// edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+// update route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+// delete route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+
+  res.redirect("/listings");
+});
+
+// Show Listing Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My Home",
+//     description: "By the Highway",
+//     price: 1200,
+//     location: "Agra",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log(`sample was save`);
+//   res.send("successful");
+// });
 
 main()
   .then(() => {
