98c2684fe760cb971d7c8e802495fa0f59b92d40
agrajjain4786 <agrajjain4786@gmail.com>
2025-09-03 11:19:29 +0530

    demo of ejs-mate

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const method = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(method("_method"));
+app.engine("ejs", ejsMate);
+
 app.get("/", (req, res) => {
   res.send("root");
 });
