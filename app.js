51f30d1a4c13411acd07e0d3504e251e79bc46c8
agrajjain4786 <agrajjain4786@gmail.com>
2025-09-10 18:23:56 +0530

    redoring error page

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = `Something went wronge` } = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs", { err });
 });
 
 let port = 8080;
