fd90210f8154beec83ef82e08a57a16909a95aa5
agrajjain4786 <agrajjain4786@gmail.com>
2025-09-16 15:35:28 +0530

    making login ("/login)and signup("/signup) page with passport

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listings.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listings.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -36,15 +40,32 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.deleted = req.flash("deleted");
-  res.locals.notExist = req.flash("notExist");
+  res.locals.error = req.flash("error");
   next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.get("/demouser", async (req, res, next) => {
+  let fakeUser = new User({
+    email: "abcd@gmail.com",
+    username: "abcd",
+  });
+  let registerdUser = await User.register(fakeUser, "password");
+  res.send(registerdUser);
+});
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 main()
   .then(() => {
---
+++ routes/user.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+router.post(
+  "/signup",
+  wrapAsync(async (req, res, next) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registerUser = await User.register(newUser, password);
+      console.log(registerUser);
+      req.flash("success", "Welcome to GustHub");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res, next) => {
+    let { username } = req.body;
+    req.flash("success", `Hi ${username} , Welcome back to GustHub`);
+    res.redirect("/listings");
+  }
+);
+
+module.exports = router;
