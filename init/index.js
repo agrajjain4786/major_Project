08f1f972029a75e950db77f65c326d620d3484ff
agrajjain4786 <agrajjain4786@gmail.com>
2025-09-17 17:11:54 +0530

    add sample listing's owner

--- init/index.js
+++ init/index.js
@@ -15,6 +15,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "68c9354d0b510998f853c58b",
+  }));
   await Listing.insertMany(initData.data);
   console.log(`data was initialized`);
 };
